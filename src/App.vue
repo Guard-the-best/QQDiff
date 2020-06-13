c2e34cacb8f6da1de7255fbe8ec746194c96ec52
int mian <w923959419@gmail.com>
2020-06-13 12:35:13 +0800

    toolbar test push

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,33 @@
 <template>
   <div id="app">
-    <products/>
-    <!-- <carousel/> -->
-    <!-- <container/> -->
+    <topBar/>
+    <!-- <products/> -->
+    <carousel/>
+    <carousel/>
+    <!-- <mainContainer/> -->
     <!-- <loginAndRegister loginErrorMsg=""/> -->
     <!-- <welcome/> -->
   </div>
 </template>
 
 <script>
-import products from './components/content/products'
-// import carousel from './components/content/carousel'
-// import container from './components/util/main-container'
+import topBar from './components/util/top-bar'
+// import products from './components/content/products'
+import carousel from './components/content/carousel'
+// import mainContainer from './components/util/main-container'
 // import welcome from './components/welcome.vue';
 // import loginAndRegister from './components/content/login-and-register.vue';
 
 export default {
   name: 'App',
   components: {
-    // carousel,
-    // container,
+    carousel,
+    // mainContainer,
     // welcome,
     // login,
     // loginAndRegister,
-    products,
+    // products,
+    topBar,
   }
 }
 </script>
--- src/components/util/top-bar.vue
+++ src/components/util/top-bar.vue
@@ -1,70 +1,64 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-    <div class="container">
-        <a class="navbar-brand js-scroll-trigger" onclick="searchPage('/toCarousel')" href="javascript:void(0)">首页</a>
-        <button class="navbar-toggler navbar-toggler-right"
-                type="button" data-toggle="collapse"
-                data-target="#navbarResponsive"
-                aria-controls="navbarResponsive"
-                aria-expanded="false" aria-label="Toggle navigation">
-            菜单
-            <i class="fas fa-bars"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item dropdown">
-                    <a class="nav-link js-scroll-trigger dropdown-toggle" data-toggle="dropdown" href="#about">宠物<span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" onclick="searchPage('/viewCategory?categoryId=')" href="javascript:void(0)">鹦鹉</a></li>
-                        <li><a class="dropdown-item" onclick="searchPage('/viewCategory?categoryId=')" href="javascript:void(0)">猫猫</a></li>
-                        <li><a class="dropdown-item" onclick="searchPage('/viewCategory?categoryId=101')" href="javascript:void(0)">狗狗</a></li>
-                        <li><a class="dropdown-item" onclick="searchPage('/viewCategory?categoryId=')" href="javascript:void(0)">鱼儿</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link js-scroll-trigger dropdown-toggle" data-toggle="dropdown" href="#about">个人中心<span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" onclick="searchPage('/toCart')" href="javascript:void(0)">购物车 &nbsp;<span id="cartLen" class="badge badge-secondary" onclick="updateLength()">${cartLength}</span></a></li>
-                        <li><a class="dropdown-item" onclick="searchPage('/toModifyInfoServlet')" href="javascript:void(0)">个人信息</a></li>
-                        <li><a class="dropdown-item" onclick="searchPage('/toOrderPage')" href="javascript:void(0)">我的订单</a></li>
-                        <li><a class="dropdown-item" onclick="searchPage('/toChangePassword')" href="javascript:void(0)">修改密码</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#">销售情况</a>
-                </li>
-                <li class="nav-item">
-<!-- <%--                    搜索--%> -->
-                    <br />
-    <div class="input-group">
-        <input onblur="checkSearchInfo()" id="searchInfo" name="searchInfo" type="text" class="form-control" placeholder="Search...">
-        <div class="input-group-append">
-            <button class="btn btn-secondary" onclick="searchPage7()" href="javascript:void(0)">
-
-        </button>
-        </div>
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <a class="navbar-brand js-scroll-trigger" onclick="searchPage('/toCarousel')" href="javascript:void(0)">首页</a>
+    <button class="navbar-toggler navbar-toggler-right"
+            type="button" data-toggle="collapse"
+            data-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
+            aria-expanded="false" aria-label="Toggle navigation">
+        菜单
+        <i class="fas fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto" id="navbarList">
+            <li class="nav-item dropdown">
+                <a class="nav-link js-scroll-trigger dropdown-toggle" data-toggle="dropdown" href="#about">宠物<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" onclick="searchPage('/viewCategory?categoryId=')" href="javascript:void(0)">鹦鹉</a></li>
+                    <li><a class="dropdown-item" onclick="searchPage('/viewCategory?categoryId=')" href="javascript:void(0)">猫猫</a></li>
+                    <li><a class="dropdown-item" onclick="searchPage('/viewCategory?categoryId=101')" href="javascript:void(0)">狗狗</a></li>
+                    <li><a class="dropdown-item" onclick="searchPage('/viewCategory?categoryId=')" href="javascript:void(0)">鱼儿</a></li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link js-scroll-trigger dropdown-toggle" data-toggle="dropdown" href="#about">个人中心<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" onclick="searchPage('/toCart')" href="javascript:void(0)">购物车 &nbsp;<span id="cartLen" class="badge badge-secondary" onclick="updateLength()">${cartLength}</span></a></li>
+                    <li><a class="dropdown-item" onclick="searchPage('/toModifyInfoServlet')" href="javascript:void(0)">个人信息</a></li>
+                    <li><a class="dropdown-item" onclick="searchPage('/toOrderPage')" href="javascript:void(0)">我的订单</a></li>
+                    <li><a class="dropdown-item" onclick="searchPage('/toChangePassword')" href="javascript:void(0)">修改密码</a></li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#">销售情况</a>
+            </li>
+            <!-- <li class="nav-item"> -->
+                <!-- 搜索 -->
+                
+                <!-- <div> -->
+                    <!-- <label> -->
+                    <!-- <c:if test="${productLength!=0}">
+                        <c:forEach items="${prodcut}" var="productSearch">
+                    <span><a onclick="searchPage('animalItemServlet')" href="javascript:void(0)" id="${productSearch.itemId}">
+                            ${productSearch.productName}
+                    </a></span>
+                        </c:forEach>
+                    </c:if> -->
+                    <!-- TODO: 搜索框自动补齐 -->
+                    <!-- </label> -->
+                <!-- </div> -->
+            <!-- </li> -->
+            <!-- <li class="nav-item">
+                <a class="nav-link js-scroll-trigger"  href="/logout">退出登录</a>
+                !--退出登录直接跳转到欢迎界面
+            </li> -->
+        </ul>
+        <form class="form-inline navbar-right" role="search">
+            <input onblur="checkSearchInfo()" id="searchInfo" name="searchInfo" type="text" class="form-control" placeholder="Search...">
+            <button class="btn btn-secondary" onclick="searchPage7()" href="javascript:void(0)"/>
+        </form>
     </div>
-    <div>
-        <label>
-        <!-- <c:if test="${productLength!=0}">
-            <c:forEach items="${prodcut}" var="productSearch">
-        <span><a onclick="searchPage('animalItemServlet')" href="javascript:void(0)" id="${productSearch.itemId}">
-                ${productSearch.productName}
-        </a></span>
-            </c:forEach>
-        </c:if> -->
-        <!-- TODO: 搜索框自动补齐 -->
-        </label>
-    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger"  href="/logout">退出登录</a>
-<!--                    退出登录直接跳转到欢迎界面 -->
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+</nav> 
 </template>
 
 <script>
@@ -81,3 +75,6 @@ export default {
     name: 'topBar'
 }
 </script>
+
+<style>
+</style>
